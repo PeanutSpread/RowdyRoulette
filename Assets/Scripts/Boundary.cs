94d54f99a39d944d8f1d368c43197111a355b008
Adrian Piwin <adrian7048@gmail.com>
2023-11-29 12:32:55 -0500

    Dropping a card off the table respawns it infront of you

--- Assets/Scripts/Boundary.cs
+++ Assets/Scripts/Boundary.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class Boundary : MonoBehaviour
 {
     // If a card falls out of bounds, put it in front of owned player
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.gameObject.tag == "Card")
+        if (other.gameObject.tag == "Card")
         {
             GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
             foreach (var player in players)
             {
-                if (player.GetComponent<PlayerController>().player.getID() == collision.gameObject.GetComponent<CardComponent>().GetOwner()) 
+                var test = other.transform.parent.GetComponent<CardComponent>();
+                if (player.GetComponent<PlayerController>().player.getID() == other.transform.parent.GetComponent<CardComponent>().GetOwner())
                 {
-                    collision.gameObject.transform.position = player.transform.position + new Vector3(0, 0, 1);
+                    other.gameObject.transform.position = player.transform.position + new Vector3(0, 0, 1);
                 }
             }
         }
